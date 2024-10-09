b749d637205477884b5ffe4a87bac2d80d830f83
sandhyachan <sandhyachan6@gmail.com>
2024-10-09 17:58:15 +0530

    Installed bootstrap and added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Artshopbanner from './Components/Banner';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container-fluid'>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <span className="navbar-brand mb-0 h1">HC Art Shop</span>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+    <form className="d-flex">
+      <button className="btn btn-lg btn-light" type="submit">
+        <i className="bi-cart-fill me-1"></i>Cart
+        <span className="badge bg-light text-black ms-1 rounded-pill">0</span>
+      </button>
+    </form>
+  </div><div></div>
+</nav>
+      <Artshopbanner/>
+    </div>
   )
 }
 
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,9 @@
+import artshopHeader from '../assets/artshopheader.png';
+
+export default function Artshopbanner() {
+    return (
+        <>
+            <img src={artshopHeader} alt="Art Shop Header" style={{ maxWidth: '100%', height: 'auto' }}/>
+        </>
+    );
+}
